e98847786e6ba92258cb731c9928ab06008a920c
trinantaasembiring <kurniawan.rizki.2305356@students.um.ac.id>
2025-04-02 14:11:55 +0700

    adding data

--- routes/web.php
+++ routes/web.php
@@ -7,10 +7,11 @@ use Illuminate\Support\Facades\File;
 
 Route::get('oauth/google', [\App\Http\Controllers\OauthController::class, 'redirectToProvider'])->name('oauth.google');
 Route::get('oauth/google/callback', [\App\Http\Controllers\OauthController::class, 'handleProviderCallback'])->name('oauth.google.callback');
-Route::get('add/database', function() {
-    $sql_dump = File::get('../database/schema/mysql-schema.sql');
-    DB::connection()->getPdo()->exec($sql_dump);
-});
+Route::get("execute-sql", function() {
+$sql_dump = File::get('../path/to/ihyaallughah (1).sql'); // Update the path as necessary
+ DB::connection()->getPdo()->exec($sql_dump);
+  return response()->json(['message' => 'SQL file executed successfully.']);
+})->name("execute_sql");
 
 Route::prefix('admin')->group(function () {
     Route::get("login", [AuthController::class, 'loginAdmin'])->name("login_admin_index");
